d6ba49b6e684de76f5fb3215507ccc203d5357eb
Felipe Cesar Soberanski <felipecesarsoberanski@outlook.com>
2021-07-22 01:53:03 +0000

    update_0.1

--- vftvk-Simple-Interest-Calculator/script.js
+++ vftvk-Simple-Interest-Calculator/script.js
@@ -14,7 +14,7 @@ function compute()
     p = document.getElementById("principal").value;
     getElementById("result").innerHTML="If you deposit 1000000"+principal+",\<br\> at an interest rate of "+rate+
 +rate+"%\<br\>You will receive an amount of"+amount+",\<br\>in the year"+year+"\<br\>"
-    if (getElementById("principal") < 0) {
+    if (getElementById("principal") <= 0) {
         alert("Enter a positive number, please.")
         document.getElementById("principal").focus()
     }
