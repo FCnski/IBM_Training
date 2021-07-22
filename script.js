a33eda87fc9ae0b52ff362a1f8c754d8c0d7a67e
Felipe Cesar Soberanski <felepeipe@gmail.com>
2021-07-21 23:47:21 -0300

    Replacement with working files

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
 
 function updateRate() {
     var rateval = document.getElementById("rate").value;
@@ -11,7 +6,21 @@ function updateRate() {
 
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+        if (principal <= 0) {
+
+        alert("Enter a positive number, please.")
+        document.getElementById("principal").focus()
+    } else {
+
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\> at an interest rate of "+rate+
+        "%\<br\> You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    }
+
 }
         
